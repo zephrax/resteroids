5aa8728a5ea340fb5cecad4c063c6718b2a07838
zephrax <zephrax@gmail.com>
2016-07-24 12:38:09 -0300

    CLI new

--- bin/commands/new.js
+++ bin/commands/new.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs-extra'));
+const path = require('path');
+const debug = require('debug')('resteroid:command:new');
+
 const Command = require('../../lib/cli/base-command');
 
 class CommandNew extends Command {
@@ -11,7 +16,15 @@ class CommandNew extends Command {
   }
 
   handler(params) {
+    const source = path.resolve(`${__dirname}../../../blueprints/app`);
+    const target = path.resolve(this._args._[0] ? this._args._[0] : process.cwd());
 
+    debug(source);
+    debug(target);
+
+    fs.copy(source, target, (err, results) => {
+      this.output('Project bootstraped');
+    });
   }
 
 }
